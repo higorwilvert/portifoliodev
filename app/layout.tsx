01c3c483ad8437ccbd6a7df78b360d55957c1365
higorwilvert <higorwilvert16@gmail.com>
2025-06-09 19:31:00 -0300

    removido linha desnecessaria

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Higor Wilvert | Desenvolvedor",
   description: "Desenvolvedor focado em soluções eficientes e interfaces modernas",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
